34e4aef0e2e9e1f8fed616c918708bd87feea870
Tomohito Ozaki <ozaki@yuroyoro.com>
2013-03-26 20:27:53 +0900

    Add spec about Class#/ and UnboundMethod#<

---
+++ spec/class_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+describe 'class' do
+  describe './' do
+    subject { String / :index }
+
+    it { should be_a_kind_of UnboundMethod }
+    it("(klass / method_name).bind(obj).call(x) should be obj.method_name(x)"){
+      subject.bind("foobarbaz").call("bar") == "foobarbaz".index("bar")
+    }
+  end
+end
---
+++ spec/unbound_method_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe UnboundMethod do
+  describe '#<' do
+    let(:unbound_method){ String.instance_method(:index) }
+    subject { unbound_method < "foobarbaz" }
+
+    it { should be_a_kind_of Method }
+    it('(unbound_method < obj).call(x) should be unbound_method.bind(obj).call(x)'){
+      subject.call('bar').should == unbound_method.bind("foobarbaz").call("bar")
+    }
+  end
+end
