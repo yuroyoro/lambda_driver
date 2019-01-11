5480e43f4b26f681ab6fbee6034b38243d4da410
Tomohito Ozaki <ozaki@yuroyoro.com>
2019-01-11 15:43:24 +0900

    proc composition `compose` now delegates to Proc#<< supported from Ruby 2.6
    Ruby 2.6 Proc#>> implentation does not concern passed object is callable.
    
    So the following code chains `>>` with Symbol raises error on Ruby 2.6
    Becase of Ruby2.6's Proc#>> does not convert passed symbol
    to proc when combine it but lambda_driver does.
    
    ```
      arr.map(&:to_s >> :upcase >> :to_sym)
      # =>(undefined method `call' for :to_sym:Symbol)
    ```
    
    To fix this behaivor, lambda_driver override Proc#>> and Proc#<<

--- lib/lambda_driver/composable.rb
+++ lib/lambda_driver/composable.rb
@@ -1,27 +1,56 @@
 # -*- encoding : utf-8 -*-
 module LambdaDriver::Composable
-  # Returns new lambda which composed self and given function.
-  # A composed proc called with args, executes `self.(g(*args)).
-  #
-  #   f = lambda{|x| x.to_s }
-  #   g = lambda{|y| y.length }
-  #   h = f compose g
-  #   h.(:hoge) # => 4
-  #
-  # This method is aliased as `<<`.
-  #
-  #   f << g # => f.compose(g)
-  #
-  def compose(g)
-    lambda{|*args| self.to_proc.call(g.to_proc.call(*args)) }
-  end
+  if RUBY_VERSION < "2.6.0"
+    # Returns new lambda which composed self and given function.
+    # A composed proc called with args, executes `self.(g(*args)).
+    #
+    #   f = lambda{|x| x.to_s }
+    #   g = lambda{|y| y.length }
+    #   h = f compose g
+    #   h.(:hoge) # => 4
+    #
+    # This method is aliased as `<<`.
+    #
+    #   f << g # => f.compose(g)
+    #
+    def compose(g)
+      lambda{|*args|
+        self.to_proc.call(g.to_proc.call(*args))
+      }
+    end
 
-  # g compose self
-  def >>(g)
-    g.to_proc << self
-  end
+    # g compose self
+    def >>(g)
+      g.to_proc << self
+    end
 
-  def self.included(klass)
-    klass.send(:alias_method, :<<, :compose)
+    def self.included(klass)
+      klass.send(:alias_method, :<<, :compose)
+    end
+  else
+    # Returns new lambda which composed self and given function.
+    # A composed proc called with args, executes `self.(g(*args)).
+    #
+    #   f = lambda{|x| x.to_s }
+    #   g = lambda{|y| y.length }
+    #   h = f compose g
+    #   h.(:hoge) # => 4
+    #
+    # This method is aliased as `<<`.
+    #
+    #   f << g # => f.compose(g)
+    #
+    def compose(g)
+      self.to_proc << g.to_proc
+    end
+
+    # g compose self
+    def >>(g)
+      g.to_proc << self
+    end
+
+    def self.included(klass)
+      klass.send(:alias_method, :<<, :compose)
+    end
   end
 end
